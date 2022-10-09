d240518c51a87c1f94d7c4ce5f6a923a426085cb
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-09 17:47:50 +0200

    error fixed with js variable initialized

--- style.js
+++ style.js
@@ -1,8 +1,13 @@
 let start = document.getElementById('#start');
 let stop = document.getElementById('#stop');
-
+let hour = document.getElementById('#hour');
+let minute = document.getElementById('#minute');
+let second = document.getElementById('#second');
+let condition = false;
 function starts(){
     console.log(' start click works');
+
+
 }
 function stops(){
     console.log('stop click works');
