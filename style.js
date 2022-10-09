f017253ad563d6299b267de9feccb96eb65c2f06
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-09 19:43:58 +0200

    second count

--- style.js
+++ style.js
@@ -1,14 +1,19 @@
-let start = document.getElementById('#start');
-let stop = document.getElementById('#stop');
-let hour = document.getElementById('#hour');
-let minute = document.getElementById('#minute');
-let second = document.getElementById('#second');
-let condition = false;
-function starts(){
-    console.log(' start click works');
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
 
+start.addEventListener("click",starts);
 
-}
-function stops(){
-    console.log('stop click works');
+function starts (){
+    let counter =0;
+    setInterval(() => {
+        counter++;
+        second.innerHTML = counter;
+        if(counter === 60){
+           
+            counter =0;
+        }
+    }, 1000);
 }
