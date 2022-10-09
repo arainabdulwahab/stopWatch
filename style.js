95dcb9666b5b58130e67f3b56181e2ab5d23a5e0
arainabdulwahab <arainabdulwahab6@gmail.com>
2022-10-09 17:42:00 +0200

    onclick function console.log

---
+++ style.js
@@ -0,0 +1,9 @@
+let start = document.getElementById('#start');
+let stop = document.getElementById('#stop');
+
+function starts(){
+    console.log(' start click works');
+}
+function stops(){
+    console.log('stop click works');
+}
